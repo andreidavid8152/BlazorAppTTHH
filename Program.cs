ec06204e0c92dd6dbc3ddd7b7c85a822a4b98136
andreidavid8152 <andreidavid3399@gmail.com>
2023-12-05 22:14:59 -0500

    funcionando la lista de empresas desde el API

--- Program.cs
+++ Program.cs
@@ -1,9 +1,19 @@
+using BlazorAppTTHH.Services;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Configuracion HttpClient con Url de la API
+builder.Services.AddHttpClient("ApiService", client =>
+{
+    client.BaseAddress = new Uri(builder.Configuration["ApiSettings:BaseUrl"]);
+});
+
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+builder.Services.AddScoped<APITTHH>();
+
 
 var app = builder.Build();
 
@@ -13,6 +23,6 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.MapBlazorHub();
-app.MapFallbackToPage("/login");
+app.MapFallbackToPage("/_Host");
 
 app.Run();
---
+++ Services/APITTHH.cs
@@ -0,0 +1,32 @@
+﻿using BlazorAppTTHH.Models;
+using System.Net.Http.Json;
+
+
+namespace BlazorAppTTHH.Services
+{
+    public class APITTHH
+    {
+        private readonly HttpClient _httpClient;
+
+        public APITTHH(IHttpClientFactory clientFactory)
+        {
+            _httpClient = clientFactory.CreateClient("ApiService");
+        }
+
+        public async Task<List<Emisor>> GetEmisores()
+        {
+            var response = await _httpClient.GetAsync("Varios/GetEmisor"); // endpoint de emisores
+            response.EnsureSuccessStatusCode();
+
+            var emisores = await response.Content.ReadFromJsonAsync<List<Emisor>>();
+
+            // Enviar mensaje en caso de que los emisores no tengan contenido.
+            if (emisores == null)
+            {
+                throw new InvalidOperationException("No se recibieron datos de emisores.");
+            }
+
+            return emisores;
+        }
+    }
+}
