535d5ef90b350c0d12d740aea62171fb59816083
andreidavid8152 <andreidavid3399@gmail.com>
2023-12-12 19:13:38 -0500

    using System.Text.Json.Serialization;

--- Models/UsuarioResponse.cs
+++ Models/UsuarioResponse.cs
@@ -1,10 +1,15 @@
-﻿namespace BlazorAppTTHH.Models
+﻿using System.Text.Json.Serialization;
+
+namespace BlazorAppTTHH.Models
 {
     public class UsuarioResponse
     {
+        [JsonPropertyName("OBSERVACION")]
         public string Observacion { get; set; }
         public string Emisor { get; set; }
+        [JsonPropertyName("PERFIL")]
         public string Perfil { get; set; }
+        [JsonPropertyName("NOMBREEMISOR")]
         public string NombreEmisor { get; set; }
     }
 }
