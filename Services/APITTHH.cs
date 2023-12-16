3167ae29d1f91fdc025716815aa703fc0743ca9a
andreidavid8152 <andreidavid3399@gmail.com>
2023-12-15 21:55:49 -0500

    login concluido y validado

--- Services/APITTHH.cs
+++ Services/APITTHH.cs
@@ -1,5 +1,7 @@
 ﻿using BlazorAppTTHH.Models;
+using Newtonsoft.Json;
 using System.Net.Http.Json;
+using System.Text.Json.Serialization;
 using System.Web;
 
 namespace BlazorAppTTHH.Services
@@ -41,12 +43,19 @@ namespace BlazorAppTTHH.Services
             var response = await _httpClient.GetAsync($"Usuarios?{queryString}");
             response.EnsureSuccessStatusCode();
 
-            var usuario = await response.Content.ReadFromJsonAsync<UsuarioResponse>();
-            if (usuario == null)
+            var content = await response.Content.ReadAsStringAsync();
+
+            // Cambio aquí para deserializar como una lista
+            var usuarios = JsonConvert.DeserializeObject<List<UsuarioResponse>>(content);
+
+            if (usuarios == null || !usuarios.Any())
             {
                 throw new InvalidOperationException("Ocurrió un error.");
             }
-            return usuario;
+
+            // Puedes decidir qué hacer si hay múltiples usuarios, por ejemplo, devolver el primero
+            return usuarios.FirstOrDefault();
+
         }
 
 
